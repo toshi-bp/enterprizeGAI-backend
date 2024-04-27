0159e711ae70a1ae4f33234930ead24da8e5f9be
Tomoya148665 <kopeyama27@gmail.com>
2024-04-28 00:04:39 +0900

    fix: サイドバーのポチの位置を修正

--- frontend/src/components/Center.tsx
+++ frontend/src/components/Center.tsx
@@ -183,18 +183,18 @@ const Center: React.FunctionComponent<CenterProps> = ({
         </div>
       </div>{" "}
       <button
-        className={`absolute cursor-pointer border-none p-0 bg-transparent top-1/2 left-3 ${
+        className={`fixed cursor-pointer border-none p-0 bg-transparent top-1/2 -translate-y-1/2  ${
           isLeftbarOpen
-            ? "bg-[url('/public/Sidebutton@3x.png')]"
-            : "bg-[url('/public/CurvedSidebutton@3x.png')]"
+            ? "bg-[url('/public/Sidebutton@3x.png')] left-64"
+            : "bg-[url('/public/CurvedSidebutton@3x.png')] left-3"
         } bg-contain bg-no-repeat h-[26px] w-[10px] hover:brightness-50 hover:contrast-200`}
         onClick={toggleLeftbar}
       ></button>
       <button
-        className={`absolute cursor-pointer border-none p-0 bg-transparent top-1/2 right-3 ${
+        className={`fixed cursor-pointer border-none p-0 bg-transparent top-1/2 -translate-y-1/2 ${
           isRightbarOpen
-            ? "bg-[url('/public/Sidebutton@3x.png')]"
-            : "bg-[url('/public/RightCurvedSidebutton@3x.png')]"
+            ? "bg-[url('/public/Sidebutton@3x.png')] right-[246px]"
+            : "bg-[url('/public/RightCurvedSidebutton@3x.png')] right-3"
         } bg-contain bg-no-repeat h-[26px] w-[10px] hover:brightness-50 hover:contrast-200`}
         onClick={toggleRightbar}
       ></button>
